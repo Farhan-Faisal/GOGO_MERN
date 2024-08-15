82cc983794f95f9cb5447aae10077ed4d3a2c350
Farhan-Faisal <farhan.faisal@mail.utoronto.ca>
2024-08-15 14:51:34 +0000

    server CI/CD

--- setup/server/server.js
+++ setup/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
+const app = express(); ///
 const port = process.env.PORT || 5000;
 const passport = require("passport");
 
