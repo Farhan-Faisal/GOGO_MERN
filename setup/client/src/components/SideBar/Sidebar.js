3c8073999988c189fbd6492f909a3295c4b1d6f9
Fahrin0802 <farhan.faisal@mail.utoronto.ca>
2024-08-19 23:57:55 -0400

    ssl

--- setup/client/src/components/SideBar/Sidebar.js
+++ setup/client/src/components/SideBar/Sidebar.js
@@ -7,46 +7,55 @@ import jwtDecode from "jwt-decode";
 
 function Sidebar({ isBusiness }) {
   const [data, setData] = useState(SidebarData);
-  const [trigger, setTrigger] = useState(false);
+  const [isOpen, setIsOpen] = useState(false); // New state to manage sidebar toggle
 
   useEffect(() => {
     const rawToken = localStorage.getItem("token");
-    console.log(rawToken);
-    if (rawToken !== null)
-    {
+    if (rawToken !== null) {
       const token = jwtDecode(localStorage.getItem("token"));
       setData(SidebarData);
     }
   }, [isBusiness]);
 
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={sidebarStyles.sidebar}>
-      <div className={sidebarStyles.gogoLogo}>
-        <p>GoGo</p>
+    <div>
+      <div className={sidebarStyles.hamburger} onClick={toggleSidebar}>
+        {/* Hamburger Icon */}
+        <div className={sidebarStyles.bar}></div>
+        <div className={sidebarStyles.bar}></div>
+        <div className={sidebarStyles.bar}></div>
       </div>
-      <br></br>
-      <nav>
-        <ul style={{ listStyleType: "none" }}>
-          {data.map((page, index) => {
-            return (
-              <li key={index}>
-                <Link to={page.path} style={{ textDecoration: "none" }}>
-                  {page.title.localeCompare("Logout") === 0 ? (
-                    <div className={sidebarStyles.transparentButton} onClick={() => localStorage.removeItem("token")}>
-                      {page.title}
-                    </div>
-                  ):(
-                    <div className={sidebarStyles.transparentButton}>
-                      {page.title}
-                    </div>
-                    )
-                  }
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+
+      <div className={`${sidebarStyles.sidebar} ${isOpen ? sidebarStyles.open : ''}`}>
+        <div className={sidebarStyles.gogoLogo}>
+          <p>GoGo</p>
+        </div>
+        <nav>
+          <ul style={{ listStyleType: "none" }}>
+            {data.map((page, index) => {
+              return (
+                <li key={index}>
+                  <Link to={page.path} style={{ textDecoration: "none" }} onClick={toggleSidebar}>
+                    {page.title.localeCompare("Logout") === 0 ? (
+                      <div className={sidebarStyles.transparentButton} onClick={() => localStorage.removeItem("token")}>
+                        {page.title}
+                      </div>
+                    ) : (
+                      <div className={sidebarStyles.transparentButton}>
+                        {page.title}
+                      </div>
+                    )}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
